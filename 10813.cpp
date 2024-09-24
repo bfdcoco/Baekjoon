47cca0ec6c03769f349dee02d9494a092f575924
bfdcoco <57896951+bfdcoco@users.noreply.github.com>
2024-09-24 21:20:01 +0900

    10813
    Try : 1
    
    Level of difficulty : Easy

---
+++ 10813.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	
+	int N, M;
+
+	int Selected_Basket, Changed_Basket, Tmp;
+	
+	int Basket_list[100] = {0,};
+
+	cin >> N >> M;
+
+	// 바구니 만들고 공 넣기
+	for (int i = 0; i < N; i++) {
+		Basket_list[i] = i+1;
+	}
+
+	for (int i = 0; i < M; i++) {
+		cin >> Selected_Basket >> Changed_Basket; 
+		Tmp = Basket_list[Selected_Basket-1];
+		Basket_list[Selected_Basket-1] = Basket_list[Changed_Basket-1];
+		Basket_list[Changed_Basket-1] = Tmp;
+	}
+
+	for (int i = 0; i < N; i++) {
+		cout << Basket_list[i] << " ";
+	}
+
+	return 0;
+}
